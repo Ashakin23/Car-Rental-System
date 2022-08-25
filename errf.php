f882755837623d9fde825da49c65fd4e8bdd3669
Ashakin-sus <kazihabibul2@gmail.com>
2022-08-25 20:55:45 +0600

    fixed error(probably)

--- errf.php
+++ errf.php
@@ -34,16 +34,54 @@ function psmth($password,$cpassword){
 }
 
 function unameexists($conn,$username){
+	// $restf;
+	// $sql = "SELECT username FROM user_cred;";
+	// $res = mysqli_query($conn,$sql);
+
+	// while ($row = mysqli_fetch_assoc($res)){
+	// 	if ($row['username']=== $username){
+	// 		$restf = true;
+	// 	}
+	// 	else{
+	// 		$restf = false;
+	// 	}
+		
+	// }
+	// return $restf;
 	$restf;
-	$sql = "SELECT username FROM user_cred;";
+	if(mysqli_num_rows(mysqli_query($conn, "SELECT username FROM user_cred WHERE username = '$username'"))){
+		$restf = true;
+		return $restf;
+	}
+	else{
+		$restf = false;
+		return $restf;
+	}
+
+}
+
+function emptyFieldlogin($usOrem,$password){
+	$restf;
+	if (empty($usOrem) || empty($password)){
+		$restf = true;
+	}
+	else {
+		$restf = false;
+	}
+	return $restf;
+}
+
+function emailexistslogin($conn,$usOrem){
+	$echk;
+	$sql = "SELECT email FROM user_cred;";
 	$res = mysqli_query($conn,$sql);
 
 	while ($row = mysqli_fetch_assoc($res)){
-		if ($row['username']=== $username){
-			$restf = true;
+		if ($row['email']=== $usOrem){
+			$echk = true;
 		}
 		else{
-			$restf = false;
+			$echk = false;
 		}
 		
 	}
