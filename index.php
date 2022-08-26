89f16754830e51a9a67fca37b9dbfaac2c7fb880
Ankhi Akter Mim <60749582+AnkhiMim@users.noreply.github.com>
2022-08-26 16:51:55 +0600

    updated index

--- index.php
+++ index.php
@@ -1,28 +1,181 @@
 <!doctype html>
-<html>
-	<head>
-		<meta charset="utf-8">
-		<meta name = "viewport" content= "width= device-width" initial-scale="1.0">
-		<title>Car Rental</title>
-		
-	</head>
-	<body>
-		<div>
-		<!--<form action="http://localhost:4000/projects/rental_login/new/Login.php">
-  			
-  			input type="text" id="uname" name="uname" placeholder="Nam de"><br>
-  			<input type="password" id="pass" name="pass" placeholder="Chabi de"></br>
-  			<input type="submit" value="LOGIN">
-		</form>-->
-		<a href="http://localhost/new/Login_user.php">LOGIN for users</a><br>
-		<a href="http://localhost/new/signup_user.php">Sign Up for users</a><br><br>
+<html lang="en">
 
-		<a href="http://localhost/new/Login_owner.php">LOGIN for Owners</a><br>
-		<a href="http://localhost/new/signup_owner.php">Sign Up for Owners</a>
+  <head>
+    <title>Car Rent &mdash; Free Website Template by Colorlib</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-	</div>
+    <link href="https://fonts.googleapis.com/css?family=DM+Sans:300,400,700&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="css/jquery.fancybox.min.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+    <link rel="stylesheet" href="css/aos.css">
+
+    <!-- MAIN CSS -->
+    <link rel="stylesheet" href="css/style.css?v=0.0.1">
+
+  </head>
+
+  <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+
+    
+    <div class="site-wrap" id="home-section">
+
+      <div class="site-mobile-menu site-navbar-target">
+        <div class="site-mobile-menu-header">
+          <div class="site-mobile-menu-close mt-3">
+            <span class="icon-close2 js-menu-toggle"></span>
+          </div>
+        </div>
+        <div class="site-mobile-menu-body"></div>
+      </div>
 
 
 
-	</body>
+      <header class="site-navbar site-navbar-target" role="banner">
+
+        <div class="container">
+          <div class="row align-items-center position-relative">
+
+            <div class="col-3 ">
+              <div class="site-logo">
+                <a href="index.html">CarRent</a>
+              </div>
+            </div>
+
+            <div class="col-9  text-right">
+              
+
+              <span class="d-inline-block d-lg-none"><a href="#" class="text-white site-menu-toggle js-menu-toggle py-5 text-white"><span class="icon-menu h3 text-white"></span></a></span>
+
+              
+
+              <nav class="site-navigation text-right ml-auto d-none d-lg-block" role="navigation">
+                <ul class="site-menu main-menu js-clone-nav ml-auto ">
+                  <li><a href="index.html" class="nav-link">Home</a></li>
+                  
+                  <a href="contact.html" class="ml-auto btn btn-light"><strong>Admin</strong></a>
+                </ul>
+              </nav>
+            </div>
+
+            
+          </div>
+        </div>
+
+      </header>
+
+    <div class="ftco-blocks-cover-1">
+      <div class="ftco-cover-1 overlay" style="background-image: url('images/hero_1.jpg')">
+        <div class="container">
+          <div class="row align-items-center">
+            <div class="col-lg-5">
+              <div class="feature-car-rent-box-1">
+                <strong><h3>For Client</h3></strong>
+                
+                  <a href="http://localhost/new/login_user.php" class="ml-auto btn btn-dark"><strong>Log In</strong></a>
+                  <a href="http://localhost/new/signup_user.php" class="ml-auto btn btn-dark"><strong>Sign Up</strong></a>
+                </div>
+                
+              </div>
+              <div class="col-lg-5">
+                <div class="feature-car-rent-box-1">
+                 <strong> <h3>For Owner</h3></strong>
+                  
+                 <a href="http://localhost/new/login_owner.php" class="ml-auto btn btn-dark"><strong>Log In</strong></a>
+                 <a href="http://localhost/new/signup_owner.php" class="ml-auto btn btn-dark"><strong>Sign Up</strong></a>
+                  </div>
+                  
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    
+
+    <footer class="site-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-3">
+            <h2 class="footer-heading mb-4">About Us</h2>
+                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+          </div>
+          <div class="col-lg-8 ml-auto">
+            <div class="row">
+              <div class="col-lg-3">
+                <h2 class="footer-heading mb-4">Quick Links</h2>
+                <ul class="list-unstyled">
+                  <li><a href="#">About Us</a></li>
+                  <li><a href="#">Testimonials</a></li>
+                  <li><a href="#">Terms of Service</a></li>
+                  <li><a href="#">Privacy</a></li>
+                  <li><a href="#">Contact Us</a></li>
+                </ul>
+              </div>
+              <div class="col-lg-3">
+                <h2 class="footer-heading mb-4">Quick Links</h2>
+                <ul class="list-unstyled">
+                  <li><a href="#">About Us</a></li>
+                  <li><a href="#">Testimonials</a></li>
+                  <li><a href="#">Terms of Service</a></li>
+                  <li><a href="#">Privacy</a></li>
+                  <li><a href="#">Contact Us</a></li>
+                </ul>
+              </div>
+              <div class="col-lg-3">
+                <h2 class="footer-heading mb-4">Quick Links</h2>
+                <ul class="list-unstyled">
+                  <li><a href="#">About Us</a></li>
+                  <li><a href="#">Testimonials</a></li>
+                  <li><a href="#">Terms of Service</a></li>
+                  <li><a href="#">Privacy</a></li>
+                  <li><a href="#">Contact Us</a></li>
+                </ul>
+              </div>
+              <div class="col-lg-3">
+                <h2 class="footer-heading mb-4">Quick Links</h2>
+                <ul class="list-unstyled">
+                  <li><a href="#">About Us</a></li>
+                  <li><a href="#">Testimonials</a></li>
+                  <li><a href="#">Terms of Service</a></li>
+                  <li><a href="#">Privacy</a></li>
+                  <li><a href="#">Contact Us</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+       
+      </div>
+    </footer>
+
+    </div>
+
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.sticky.js"></script>
+    <script src="js/jquery.waypoints.min.js"></script>
+    <script src="js/jquery.animateNumber.min.js"></script>
+    <script src="js/jquery.fancybox.min.js"></script>
+    <script src="js/jquery.easing.1.3.js"></script>
+    <script src="js/bootstrap-datepicker.min.js"></script>
+    <script src="js/aos.js"></script>
+
+    <script src="js/main.js"></script>
+
+  </body>
+
 </html>
+
