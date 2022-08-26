ae443f8335535169f219d32f2090501e780b2904
navid001 <navidalvi.001@gmail.com>
2022-08-26 18:52:21 +0600

    something

--- dashboard_owner.php
+++ dashboard_owner.php
@@ -14,12 +14,12 @@ session_start();
 			
 		    include_once 'loregdb.php';
 		    $username = $_SESSION['username'];
-		    $fullname = $_SESSION['fullname'];
-		    $sql = "SELECT username,email FROM owner_cred WHERE username='$username';";
+		    // $fullname = $_SESSION['fullname'];
+		    $sql = "SELECT username,email,fullname FROM owner_cred WHERE username='$username';";
 		    $res = mysqli_query($conn,$sql);
 		    $row = mysqli_fetch_assoc($res);
 
-			echo "Hello Car Owner ",$fullname,".Your email address is: ",$row['email'];
+			echo "Hello Car Owner ",$row['fullname'],".Your email address is: ",$row['email'];
 
 		?>
 
